5e740e44d39d42c3ba02bc80a1c348dcf6cab108
Pascual Juan Sánchez <pjs8@alu.ua.es>
2021-11-23 13:48:49 +0100

    Añadido componente User

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <user></user>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import User from './components/User.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    User
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/User.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <h1>Soy el usuario {{ username }}</h1>
+    <table>
+        <thead>
+            <th>Información usuario</th>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Nombre</th>
+                <td>{{nombre}}</td>
+            </tr>
+            <tr>
+                <th>Apellidos</th>
+                <td>{{apellidos}}</td>
+            </tr>
+            <tr>
+                <th>Dirección</th>
+                <td>{{direccion}}</td>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td>{{email}}</td>
+            </tr>
+            <tr>
+                <th>Fecha de nacimiento</th>
+                <td>{{fechaNac}}</td>
+            </tr>
+            <tr>
+                <th>Id</th>
+                <td>{{id}}</td>
+            </tr>
+        </tbody>
+    </table> 
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'User',
+    data() {
+        return {
+            id: 1,
+            username: 'realpascuuu',
+            email: 'pjs8@alu.ua.es',
+            nombre: 'Pascual',
+            apellidos: 'Juan',
+            direccion: 'calle esta',
+            fechaNac: '09-04-1999'
+        }
+    }
+}
+
+</script>
