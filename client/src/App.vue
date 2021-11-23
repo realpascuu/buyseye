9fd97690648d156fe0c9443d65cd1d2a701ed92e
Pascual Juan Sánchez <pjs8@alu.ua.es>
2021-11-23 14:08:22 +0100

    Inicio login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <user></user>
+  <login></login>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import User from './components/User.vue'
-
+//import User from './components/User.vue'
+import Login from './components/Login.vue'
 export default {
   name: 'App',
   components: {
-    User
+    Login
   }
 }
 </script>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="box">
+    <img src="../assets/logoletra.png">
+    <h3>Inicio sesión</h3>
+    <div>
+        <input type="text" placeholder="Nombre de usuario">
+    </div>
+    <div>
+        <input type="password" placeholder="Contraseña">
+    </div>
+    <button>Entrar</button>
+    <div>
+        <b>¿No tienes cuenta? <a href="">Regístarte aquí</a></b>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            login: '',
+            password: ''
+        }
+    }
+}
+</script>
+
+<style>
+img {
+    height: 200px;
+} 
+
+</style>
